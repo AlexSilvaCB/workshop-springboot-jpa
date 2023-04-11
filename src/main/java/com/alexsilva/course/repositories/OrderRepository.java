3186b4cd95d47b0729fd9ed21f39d524fd0b4633
AlexSilvaCB <96430292+AlexSilvaCB@users.noreply.github.com>
2023-04-11 18:55:55 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/alexsilva/course/repositories/OrderRepository.java
+++ src/main/java/com/alexsilva/course/repositories/OrderRepository.java
@@ -2,8 +2,11 @@ package com.alexsilva.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.alexsilva.course.entities.Order;
 import com.alexsilva.course.entities.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+	
 
 }
