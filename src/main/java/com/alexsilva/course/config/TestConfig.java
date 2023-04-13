923db1e2d1901ee585baea134eb2494836894690
AlexSilvaCB <96430292+AlexSilvaCB@users.noreply.github.com>
2023-04-13 11:15:47 -0300

    Category

--- src/main/java/com/alexsilva/course/config/TestConfig.java
+++ src/main/java/com/alexsilva/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.alexsilva.course.entities.Category;
 import com.alexsilva.course.entities.Order;
 import com.alexsilva.course.entities.User;
 import com.alexsilva.course.entities.enums.OrderStatus;
+import com.alexsilva.course.repositories.CategoryRepository;
 import com.alexsilva.course.repositories.OrderRepository;
 import com.alexsilva.course.repositories.UserRepository;
 
@@ -23,17 +25,25 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
 		Order o1 = new Order(null, Instant.parse("2023-03-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1); 
 		Order o2 = new Order(null, Instant.parse("2023-04-10T03:42:10Z"), OrderStatus.PAID, u2); 
-		Order o3 = new Order(null, Instant.parse("2023-04-11T15:21:22Z"), OrderStatus.PAID, u1); 
-
+		Order o3 = new Order(null, Instant.parse("2023-04-11T15:21:22Z"), OrderStatus.PAID, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/alexsilva/course/repositories/OrderRepository.java
+++ src/main/java/com/alexsilva/course/repositories/OrderRepository.java
@@ -3,10 +3,7 @@ package com.alexsilva.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alexsilva.course.entities.Order;
-import com.alexsilva.course.entities.User;
 
 public interface OrderRepository extends JpaRepository<Order, Long>{
-	
-	
 
 }
